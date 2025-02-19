b202535474d8a8cd9f1adce40e1ec5d3806f5817
decapix <decapixd@gmail.com>
2025-02-19 15:23:39 +0100

    build

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+
+from fastapi import FastAPI
+from routes import router
+import os
+import logging
+
+app = FastAPI()
+
+
+app.include_router(router)
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ backend/app/routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+import logging
+
+router = APIRouter()
+
+@router.get("/home")
+async def home(n: int = 10):
+    """home"""
+    logging.info("home call")
+    return {"who are the best ?": "us of course"}
