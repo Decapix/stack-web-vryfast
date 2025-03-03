ff8e143d06a9ebe674ef86cf0ea0c3a0df257ea3
decapix <decapixd@gmail.com>
2025-03-03 11:24:54 +0100

    add suppabase .env

--- run.sh
+++ run.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# Function to start the services in production mode
-start_services_prod() {
-    # Check if the supabase directory exists
+# Function to clone the Supabase repository if it doesn't exist
+clone_supabase_repo() {
     if [ ! -d "supabase" ]; then
         echo "Cloning the Supabase repository..."
         git clone --depth 1 https://github.com/supabase/supabase
     else
         echo "Supabase repository already cloned."
     fi
+}
 
-    cd supabase/docker
-
-    # Check if the .env file exists
+# Function to set up the .env file if it doesn't exist
+setup_env_file() {
     if [ ! -f ".env" ]; then
         echo "Copying the fake env vars..."
-        cp .env.example .env
+        cp ../../.env-supabase .env
     else
         echo ".env file already exists."
     fi
+}
 
+# Function to pull the latest Docker images and start the services
+start_docker_services() {
     echo "Pulling the latest Docker images..."
     docker compose pull
 
     echo "Starting the services in detached mode..."
     docker compose up -d
+}
 
+# Function to start the services in production mode
+start_services_prod() {
+    clone_supabase_repo
+    cd supabase/docker
+    setup_env_file
+    start_docker_services
     cd ../..
 
     echo "Starting the docker-compose-project.yaml..."
@@ -34,31 +43,12 @@ start_services_prod() {
 
 # Function to start the services in development mode
 start_services_dev() {
-    Check if the supabase directory exists
-    if [ ! -d "supabase" ]; then
-        echo "Cloning the Supabase repository..."
-        git clone --depth 1 https://github.com/supabase/supabase
-    else
-        echo "Supabase repository already cloned."
-    fi
-
+    clone_supabase_repo
     cd supabase/docker
-
-    # Check if the .env file exists
-    if [ ! -f ".env" ]; then
-        echo "Copying the fake env vars..."
-        cp .env.example .env
-    else
-        echo ".env file already exists."
-    fi
-
-    echo "Pulling the latest Docker images..."
-    docker compose pull
-
-    echo "Starting the services in detached mode..."
-    docker compose up -d
-
+    setup_env_file
+    start_docker_services
     cd ../..
+
     echo "Starting the docker-compose-project.yaml in attached mode..."
     docker compose -f docker-compose-project.yaml up
 }
@@ -105,6 +95,6 @@ case $1 in
         stop_project
         ;;
     *)
-        echo "Usage: $0 {up-prod|up-dev|restart-dev|down-all|down-project}"
+        echo "Usage: $0 {up-prod|up-dev|restart-project|down-all|down-project}"
         ;;
 esac
